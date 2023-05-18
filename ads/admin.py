6bc4f6969fbc94c769819fae22b509029707ba60
Dmitry <dimaremenec@gmail.com>
2023-05-18 14:15:06 +0300

    Category admin registration

--- ads/admin.py
+++ ads/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.db import models
 from tinymce.widgets import TinyMCE
 
-from .models import Ad, Author
+from .models import Ad, Author, Category
 
 
 class AdTinymce(admin.ModelAdmin):
@@ -13,3 +13,4 @@ class AdTinymce(admin.ModelAdmin):
 
 admin.site.register(Ad, AdTinymce)
 admin.site.register(Author)
+admin.site.register(Category)
